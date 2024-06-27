13df655c904ce444fcab678742e01679eeff3503
tamriko1010 <162357601+tamriko1010@users.noreply.github.com>
2024-06-27 21:38:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,24 @@ import sqlite3
 app = Flask(__name__)
 
 def get_db_connection():
+    # подключение к базе данных
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 def close_db_connection(conn):
+    # закрыть базу
     conn.close()
 
 def init_db():
+    # инициализация базы данных
     conn = get_db_connection()
     conn.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL)')
     conn.close()
 
 @app.route('/')
 def index():
+    # получение записей из базы
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
@@ -25,6 +29,7 @@ def index():
 
 @app.route('/<int:post_id>')
 def get_post(post_id):
+    # получение конкретной записи по id 
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     conn.close()
@@ -32,6 +37,7 @@ def get_post(post_id):
 
 @app.route('/new', methods=['GET', 'POST'])
 def new_post():
+    # добавление новой записи
     if request.method == 'POST':
         title = request.form['Заголовок']
         content = request.form['Содержание']
